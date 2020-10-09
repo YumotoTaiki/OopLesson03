61b2dc89e666c3bde983ac7e57c817227f99afd0
泰生 湯本 <31074@ojs.ac.jp>
2020-10-09 15:48:21 +0900

    練習問題6.2まで

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,41 +12,52 @@ namespace Chapter6 {
         static void Main(string[] args) {
 
             //練習問題
-            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+            var books = new List<Book> {
+                new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+                new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+                new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+                new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+                new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+                new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
+                new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+            };
 
-            //問題6.1
+            //問題6.2
             //(1)
-            var max = numbers.Where(n => n > 0).Max();
-            Console.WriteLine($"最大は{max}");
+            var book1 = books.FirstOrDefault(b => b.Title == "ワンダフル・C#ライフ");
+            if (book1 != null) {
+                Console.WriteLine($"{book1.Price} {book1.Pages}");
+            }
 
             //(2)
-            var last2 = numbers.Select(n => numbers[numbers.Length-1]);
-
-            //Console.WriteLine($"最後から2番目は{numbers[numbers.Length - 2]}");
-
-            int pos = numbers.Length - 2;
-            foreach (var number in numbers.Skip(pos)) {
-                Console.Write(number + " ");
-            }
-            Console.WriteLine();
+            int count = books.Count(b => b.Title.Contains("C#"));
+            Console.WriteLine(count);
 
             //(3)
-            var strNums = numbers.Select(name => name.ToString("000")).ToArray();
-            foreach (var item in strNums) {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
+            var average = books.Where(b => b.Title.Contains("C#")).Average(b => b.Pages);
+            Console.WriteLine(average);
 
             //(4)
-            var min3 = numbers.OrderBy(n => n).Take(3);
-            foreach (var item in min3) {
-                Console.Write(item + " ");
+            var book4 = books.FirstOrDefault(b => b.Price >= 4000);
+            if (book4 != null) {
+                Console.WriteLine(book4.Title);
             }
-            Console.WriteLine();
 
             //(5)
-            var over10 = numbers.Distinct().Count(n => n > 10);
-            Console.WriteLine($"10よりおおきいのは{over10}");
+            var pages = books.Where(b => b.Price < 4000).Max(b => b.Pages);
+            Console.WriteLine(pages);
+
+            //(6)
+            var selected = books.Where(b => b.Pages >= 4000).OrderByDescending(b => b.Price);
+            foreach (var book in selected) {
+                Console.WriteLine("{0} {1}",book.Title,book.Price);
+            }
+
+            //(7)
+            var selected7 = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500);
+            foreach (var book in books) {
+                Console.WriteLine(book.Title);
+            }
         }
     }
 }
