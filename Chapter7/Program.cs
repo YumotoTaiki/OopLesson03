c1705a0dba0ec66c7b734cf68df05182a0e17fd0
泰生 湯本 <31074@ojs.ac.jp>
2020-10-15 11:40:21 +0900

    辞書登録プログラム完成

--- Chapter7/Program.cs
+++ Chapter7/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Section02;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,70 +9,88 @@ using System.Threading.Tasks;
 
 namespace Chapter7 {
     class Program {
+
         static void Main(string[] args) {
-            #region 前半
-            //var employeeDict = new Dictionary<int, Employee> {
-            //    {100,new Employee(100,"清水遼久")},
-            //    {112,new Employee(112,"芹沢洋和")},
-            //    {125,new Employee(125,"岩瀬奈央子")},
-            //};
-            //
-            //var employees = employeeDict.Where(emp => emp.Value.Id % 2 == 0);
-            //foreach (var item in employees) {
-            //    Console.WriteLine($"{item.Value.Name}");
-            //}
 
-            //リスト
-            //List<Employee> employee = new List<Employee>() {
-            //    new Employee(100,"清水遼久"),
-            //    new Employee(112,"芹沢洋和"),
-            //    new Employee(125,"岩瀬奈央子"),
-            //    new Employee(143,"山田太郎"),
-            //    new Employee(148,"池田次郎"),
-            //    new Employee(152,"高田三郎"),
-            //    new Employee(155,"石川幸也"),
-            //    new Employee(161,"中沢信也"),
-            //};
-            //
-            //var employeeDict = employee.ToDictionary(emp => emp.Id);
-            //
-            ////KeyValuePair<int ,Employee> と var は同じ意味
-            //foreach (KeyValuePair<int ,Employee> item in employeeDict) {
-            //    Console.WriteLine($"{item.Value.Id} = {item.Value.Name}");
-            //}
-            #endregion
+            Console.WriteLine("**********************");
+            Console.WriteLine("* 辞書登録プログラム *");
+            Console.WriteLine("**********************");
 
-            //var lines = File.ReadAllLines("sample.txt");
-            //var we = new WordsExtractor(lines);
-            //foreach (var word in we.Extract()) {
-            //    Console.WriteLine(word);
-            //}
+            int num;
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>() { };
 
-            DuplicateKeySample();
+            do {
+                Console.WriteLine("1. 登録　2. 内容を表示　3. 終了");
+                var readnum = Console.ReadLine();
+                if (int.TryParse(readnum, out num)) {
+                    switch (num) {
+                        case 1:
+                            Registration(dict);
+                            break;
+                        case 2:
+                            Display(dict);
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            } while (true);
         }
 
-        static public void DuplicateKeySample() {
-            // ディクショナリの初期化
-            var dict = new Dictionary<string, List<string>>() {
-               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
-               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
-            };
+        static public void Registration(Dictionary<string, List<string>> dict) {
+            Console.Write("KEYを入力：");
+            var readKey = Console.ReadLine();
 
-            // ディクショナリに追加
-            var key = "EC";
-            var value = "電子商取引";
-            if (dict.ContainsKey(key)) {
-                dict[key].Add(value);
+            Console.Write("Valueを入力：");
+            var readValue = Console.ReadLine();
+
+            if (dict.ContainsKey(readKey)) {
+                dict[readKey].Add(readValue);
             } else {
-                dict[key] = new List<string> { value };
+                dict[readKey] = new List<string> { readValue };
             }
 
-            // ディクショナリの内容を列挙
+            Console.WriteLine("登録しました!");
+            Console.WriteLine();
+            return;
+        }
+
+        static public void Display(Dictionary<string, List<string>> dict) {
+            Console.WriteLine();
             foreach (var item in dict) {
                 foreach (var term in item.Value) {
                     Console.WriteLine("{0} : {1}", item.Key, term);
                 }
             }
+            Console.WriteLine();
         }
+
+        //static public void DuplicateKeySample() {
+        //    // ディクショナリの初期化
+        //    var dict = new Dictionary<string, List<string>>() {
+        //       { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+        //       { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+        //    };
+        //
+        //    // ディクショナリに追加
+        //    var key = "EC";
+        //    var value = "電子商取引";
+        //    if (dict.ContainsKey(key)) {
+        //        dict[key].Add(value);
+        //    } else {
+        //        dict[key] = new List<string> { value };
+        //    }
+        //
+        //    // ディクショナリの内容を列挙
+        //    foreach (var item in dict) {
+        //        foreach (var term in item.Value) {
+        //            Console.WriteLine("{0} : {1}", item.Key, term);
+        //        }
+        //    }
+        //}
+
     }
 }
